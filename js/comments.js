3ec374fec32a820886b0e1906f4c648c424ed2a8
PetaBread <petersusanto545@gmail.com>
2024-04-20 22:59:44 -0400

    progress

--- js/comments.js
+++ js/comments.js
@@ -1,4 +1,22 @@
-import { database } from './init.js';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js";
+
+// Firebase configuration for your project
+const firebaseConfig = {
+    apiKey: "AIzaSyAidhG9aOtiR1GPmcicMKC2uzFOI5SvSQ0",
+    authDomain: "si339final.firebaseapp.com",
+    projectId: "si339final",
+    storageBucket: "si339final.appspot.com",
+    messagingSenderId: "871224604451",
+    appId: "1:871224604451:web:3077444f5cccbcfcd3be96",
+    measurementId: "G-SXWP24X75P"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase();
+
 
 const pageID = document.getElementById("recipeName").innerText; // Use innerText for text content
 const commentsRef = database.ref("recipes/" + pageID);
