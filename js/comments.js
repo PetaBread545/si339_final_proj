f645ab594147420b95878aff3aada69234c8eaee
PetaBread <petersusanto545@gmail.com>
2024-04-21 00:05:01 -0400

    progress

--- js/comments.js
+++ js/comments.js
@@ -53,7 +53,7 @@ function updateComments() {
                 commentListAdd(comment);
             });
         } else {
-            document.querySelector(".commentPlaceholder").innerHTML = '<p> Be the first to leave a comment! </p>';
+            document.querySelector("#commentPlaceholder").innerHTML = '<p> Be the first to leave a comment! </p>';
         }
     });
 }
